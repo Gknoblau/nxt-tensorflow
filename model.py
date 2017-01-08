cd0f217b6bd92f721704ead4289d182105745b08
Greg Knoblauch <gknoblau@ualberta.ca>
2017-01-08 16:53:50 -0700

    Clean up

--- model.py
+++ model.py
@@ -2,53 +2,46 @@ import tensorflow as tf
 import numpy as np
 import sys
 import random
+import time
 
-dataFile = "../TF/test_data.txt"
-testFile = "../TF/test.txt"
+dataFile = "good_data_pos.txt"
 df = open(dataFile)
-tFile = open(testFile)
+
 
 examples = []
-testFile = []
+
 
 for line in df:
     data = line.split(",")
-    input = data[0:4]
+    mic = data[0:2]
+    ultra = data[2:4]
     output = data[4:6]
-    for i in range(0, len(input)):
-        input[i] = float(input[i]) / 255.0
+    for i in range(0, len(mic)):
+        mic[i] = float(mic[i]) / 100.0
+    for i in range(0, len(mic)):
+        ultra[i] = float(ultra[i]) / 255.0
     for i in range(0, len(output)):
-        output[i] = float(output[i]) / 255.0
-    examples.append((input, output))
-
-for line in tFile:
-    data = line.split(",")
-    input = data[0:4]
-    output = data[4:6]
-    for i in range(0, len(input)):
-        input[i] = float(input[i]) / 255.0
-    for i in range(0, len(output)):
-        output[i] = float(output[i]) / 255.0
-    testFile.append((input, output))
+        output[i] = float(output[i]) / 100.0
+    examples.append((mic + ultra, output))
 
 
-
-batchSize = 32
+batchSize = 16
+hiddenUnits = 10
 
 inputX = tf.placeholder(tf.float32, (batchSize, 4))
 outputY = tf.placeholder(tf.float32, (batchSize, 2))
 
-weight = tf.get_variable("mweight", (4, 10), initializer=tf.random_normal_initializer(mean=0.1))
-bias = tf.get_variable("mbias", (10, ), initializer=tf.random_normal_initializer(mean=0.1))
+weight = tf.get_variable("mweight", (4, hiddenUnits), initializer=tf.random_normal_initializer(mean=0.1))
+bias = tf.get_variable("mbias", (hiddenUnits, ), initializer=tf.random_normal_initializer(mean=0.1))
 
-weight2 = tf.get_variable("mweight2", (10, 2), initializer=tf.random_normal_initializer(mean=0.1))
+weight2 = tf.get_variable("mweight2", (hiddenUnits, 2), initializer=tf.random_normal_initializer(mean=0.1))
 bias2 = tf.get_variable("mbias2", (2, ), initializer=tf.random_normal_initializer(mean=0.1))
 
 hidden = tf.nn.relu(tf.matmul(inputX, weight) + bias)
 output = tf.nn.relu(tf.matmul(hidden, weight2) + bias2)
 
-loss = tf.reduce_sum(tf.abs(outputY - output)) # works best
-#loss = tf.nn.l2_loss(outputY - output)
+#loss = tf.reduce_sum(tf.abs(outputY - output)) # works okay
+loss = tf.nn.l2_loss(outputY - output)
 
 optimize = tf.train.AdamOptimizer(0.001).minimize(loss)
 
@@ -72,20 +65,9 @@ with tf.Session() as sess:
             batchX.append(rX)
             batchY.append(rY)
 
-
         rLoss, _ =  sess.run([loss, optimize], feed_dict={inputX: batchX, outputY: batchY})
 
         print "Loss was {}".format(rLoss)
 
         if k % 5000 == 0:
             save_path = saver.save(sess, "model.ckpt")
-        
-        # testX = []
-        # testY = []
-        # for i in range(0, batchSize):
-        #     rX, rY = random.choice(testFile)
-        #     testX.append(rX)
-        #     testY.append(rY)
-
-        # rLoss = sess.run(loss, feed_dict={inputX: testX, outputY: testY})
-        # print "Test Loss {}".format(rLoss)
